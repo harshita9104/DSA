0c5819c85c8a9c5215d95c2ac72278f71edbb87d
Harshita Roonwal <roonwal721972@gmail.com>
2025-04-21 01:55:19 +0530

    Time: 2 ms (39.87%) | Memory: 9.9 MB (79.58%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
- bool valid(char ch){
-            if((ch >= 'a' && ch <= 'z')|| (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')){
-            return 1;    
+    bool isPalindrome(string s) {
+        int st = 0;
+        int end = s.size() -1;
+        while(st<=end){
+            if(!isalnum(s[st])){
+                st++;
+                continue;
             }
-            return 0;
-        };
-        bool checkpalindrome(string str){
-            int start =0;
-        int n = str.size();
-        int end = n-1;
-        while(start <= end){
-            if(str[start] != str[end]){
+            if(!isalnum(s[end])){
+                end--;
+                continue;
+            }
+            if(tolower(s[st]) != tolower(s[end])){
                 return false;
+            }else{
+                st++;
+                end--;
             }
-            start++;
-            end--;
         }
         return true;
-        };
-        char toLower(char c){
-            if((c >= 'a' && c <= 'z') || (c >= '0' && c<= '9')){
-                return c;
-            }else{
-                char temp = c-'A' +'a';
-                return temp;
-            }
-        };
-    bool isPalindrome(string s) {
-        //sab faltu char hta do or sirf alphanumeric char ko hi tmp m push karo
-        string tmp ="";
-        for(int i = 0; i< s.size(); i++){
-            if(valid(s[i])){
-                tmp.push_back(s[i]);
-            }
-        }
-        //ab sabko lower case m convert kardo
-        //ab string tmp h to loop uski length ka chlana h
-        for(int j = 0; j< tmp.length(); j++){
-            tmp[j] = toLower(tmp[j]);
-        }
-        return checkpalindrome(tmp);
-       
-        
     }
 };
