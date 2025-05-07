2e82f7c547be4ce924879d3a03ea7644def5d7c3
Harshita Roonwal <roonwal721972@gmail.com>
2025-05-07 12:53:17 +0530

    Time: 3 ms (72.39%) | Memory: 15 MB (14.73%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //hashmap ka use krenge kyuki usme elem find or retreve krne ki time comp O(1) hoti hai extra loop nhi chlana pdega to find the remaining value from the target
-        //ek hi loop chlana hai , ek elem p khade hoke dekhna hai ki remaining value map m h kya agar hai to vahi p return kardo nhi to is elem or uske corresponding index ko map m store karke aage badho
+        //ek elem par khade hokar remaining value ko find karne m extra tc lgegi usko avoid karne k liye
+        //use hashmap jisme elem ko find krne or retrieve krne ki time complexity O(1) hoti h map wil, store elem as key and its coresponding index as value
+        //ek hi loop chlao or remaining value = (target - current elem) ko map m find kro agar mil jata hai to vahi p uska index return kardo nhi to 
+        //us elem or uske index ko map m daalke , loop m aage badho
         int n = nums.size();
-        map<int, int> mp;//stores elem, index
-        for(int i = 0; i<n; i++){
-            int rem = target - nums[i];
-            if(mp.find(rem) != mp.end()){
-                return{mp[rem],i};
+        map<int, int> mp;//elem, index
+        for(int i = 0; i<n ; i++){
+            int elem = target - nums[i];
+            if(mp.find(elem) != mp.end()){//this means elem ko find kro if doesnt point at the end of map upon looking for it , means it is present inside map so return it
+            return {i,mp[elem] };
+
+            }else{
+                mp[nums[i]] = i;
             }
-            mp[nums[i]] = i;
-
         }
-        //agar target k eq do elem nhi mile loop k end m to empty arr return kardo
-        return{};
-
+    return {}; 
     }
 };
