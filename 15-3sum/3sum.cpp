1b23664957c303f802a72a5d03b032bf317417fc
Harshita Roonwal <roonwal721972@gmail.com>
2025-05-13 21:12:12 +0530

    Time: 53 ms (44.98%) | Memory: 33.1 MB (23.74%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,42 +1,47 @@
 class Solution {
 public:
-     vector<vector<int>> result;
-    void twosum(vector<int> &nums, int target, int i , int j ){
-        while(i<j){
-            if(nums[i] + nums[j] <target){
-                i++;
-            }else if(nums[i] + nums[j] > target){
-                j--;
-            }else{
-                //to avoid duplicates while finding the numbers jab tak consecutive number same h pointer move karte raho 
-                while(i<j && nums[i] == nums[i+1]) i++;
-                while(i<j && nums[j] == nums[j-1])j--;
-                result.push_back({-target, nums[i], nums[j]});
-                i++;
-                j--;
-            }
+    vector<vector<int>> result;
+    void twosum(vector<int> &nums, int target, int j, int k){
+        while(j<k){
+         if(nums[j] + nums[k] < target){
+            j++;
+         }else if(nums[j] +  nums[k] > target){
+            k--;
+         }else {
+            //to avoid duplicates while finding the numbers jab tak consecutive number same h pointer move karte raho 
+                while(j<k && nums[j] == nums[j+1]) j++;
+                while(j<k && nums[k] == nums[k-1])k--;
+                result.push_back({-target, nums[j], nums[k]});
+                j++;
+                k--;
+         }
         }
-       
+        
     }
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-       
-        if(n < 3){
-            return {};
+        //we can think of this problem like this that 3 number need to sum up to 0 so 
+        //two number can sum to get neg of rem 1 number
+        //so keep one pointer at a number do its neg and find this -number by iterating thr the rem elem of the arr using a while loop with cond j<k as it is given tht pointer i, j, k must point at diff positions 
+    // so we can sort the arr first to simplify our serach for two sum of -number in the rem elem of arr 
+    //in this we will place j at i+1 and k at n-1 and 
+    //then compare the sum of nums[i]+ nums[j] with target which is -nums[i]  , 
+    // if this sum is smaller then we need to inc it in order to reach the target so since the arr is sorted so we need to move j++ in order to inc this sum 
+    //if the sum is larger then in order to dec this sum move k--
+    int n = nums.size();
+    if(n<3){
+        return {};
+    }
+    sort(nums.begin(), nums.end());
+    for(int i = 0; i<n; i++){
+        int target = -nums[i];
+        if(i>0 && nums[i] == nums[i-1]){
+            continue;//this is to avoid ki hm same target ko phirse search n kare arr m kyuki duplicate triplet nhi hone chahiye
         }
-        sort(nums.begin(), nums.end());
-        //fixing one elem = n1
-        for(int i = 0; i< n; i++){
-            //same n1 k liye dobara find na kare isliye
-            if(i>0 && nums[i] == nums[i-1]){
-                continue;
-            }
-            int n1 = nums[i];
-            int target = -n1;
-            //target mil gya ab rem arr par two sum lgado taaki baaki k dono elem bhi mil jae
-            twosum(nums,target, i+1, n-1);
+        //we are passing the sorted arr the target and from where to start looking for target in toe sum in every iteration
+        twosum(nums, target, i+1, n-1);
+        
+    }
+    return result;
 
-        }
-        return result;
     }
 };
