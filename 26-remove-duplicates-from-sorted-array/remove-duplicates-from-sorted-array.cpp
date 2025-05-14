d849a87d2a6d0d9ea4729ab422d55df0491fc15d
Harshita Roonwal <roonwal721972@gmail.com>
2025-05-14 14:34:11 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (78.02%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int j = 1;
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] != nums[i - 1]){
+        int n = nums.size();
+        
+        int j=1;
+        //curr p rhke chcek kro vo prev k eq to nhi h agar nhi h to hi nums[j] m vo val daalo kyuki vo uniqe hai agar equal hota to kuch mt karo aage badh jaao
+        for(int i = 1; i<n; i++){
+            if(nums[i] != nums[i-1]){
                 nums[j] = nums[i];
                 j++;
+            }else{
+                continue;//aage badho loop m kuch nhi krna
             }
         }
-        return j;
-
-        
+        return j;//this is pointing at the last unique elem in nums
     }
 };
