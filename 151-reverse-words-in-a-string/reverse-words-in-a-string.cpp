b05a8ffe27ab376425d0727b5ed43d5e2a405086
Harshita Roonwal <roonwal721972@gmail.com>
2025-05-14 17:53:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (66.54%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        //the reverse of a string with no elem is same
-        if(s.size() == 0) return s;
-        //stack should be used as in stack if we push the elem then the last elem added will be at the top of stack
-        // this staack will store strings 
         stack<string> st;
-        //and a string which will store the reversed string
-        string result;
-        //we will push the string we get in stack when we will get space or null char 
-        for(int i = 0; i<s.size(); i++){
-            string word;//before a new word starts to build after a space the word will again be intialised as a empty string 
-            if(s[i] == ' ')continue;
-            while(i<s.size() && s[i] != ' '){
-                word += s[i];
+        int n = s.size();
+        int i = 0;
+        while( i<n){
+            string word = "";//next word shuru hua 
+            while(s[i] == ' ')i++;//jab string m space hai to hmne space ko skip kiya or aage badh gye
+            while(i<n && s[i] != ' '){//build a word 
+                word+= s[i];//jab space nhi cont char hi to vo saare cont char ko ek sath ek word bnake aage badh gye 
                 i++;
             }
-            // after we have got a word from string s push it in stack
-            st.push(word);         
-        }//after this for loop the stack will have all the words in string s 
-        //now to put these words from stack to result string
-        while(!st.empty()){
-            result += st.top();//add the top word on stack in our result string
-            st.pop();//pop that word now 
-            if(!st.empty()) result+= ' ';//add a space after that word is added in result string make sure that was not the last word of stack as there is no need to add space after the last word is added in result string
-        }
-        return result;
+            // Push non-empty word
+        if (!word.empty()) st.push(word);//us word ko stack m push kiya 
 
+
+        }
+       //by the end of this loop saare word stack m push ho gye
+       string result = "";
+       while(!st.empty()){
+        result += st.top();
+        st.pop();
+        //agar vo word last word nhi hai to us word k baad ' ' daalna hoga 
+        if(!st.empty()) result += " ";
+
+       }
+        
+    return result; 
     }
 };
