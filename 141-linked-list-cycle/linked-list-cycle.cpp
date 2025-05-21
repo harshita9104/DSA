596888617e6f152fd828e9cfd36a91375f0ceb7a
Harshita Roonwal <roonwal721972@gmail.com>
2025-05-21 22:05:06 +0530

    Time: 6 ms (90.77%) | Memory: 11.6 MB (96.43%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,17 +9,26 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        map<ListNode*, bool> mp; //  Use pointer to track nodes
-        ListNode* temp = head;
-
-        while (temp != NULL) {
-            if (mp.find(temp) != mp.end()) {
-                return true; //  Cycle detected
-            }
-            mp[temp] = true;  //  Mark node as visited
-            temp = temp->next;
+        //we can also do it with 2 pointers slow and fast both starting from head slow will move one step and fast will move 2 step if at some point both will be at same position it means there is a cycle in linked list
+        if(head == NULL || head->next == NULL){
+            return false;
         }
-
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL){
+            if(slow->next != NULL){
+                slow = slow->next;
+            }
+            if(fast->next != NULL){
+            ListNode* nextnode = fast->next;
+            fast = nextnode->next;
+            }
+            
+            if(slow == fast){
+                return true;
+                break;
+            }
+        }
         return false;
     }
 };
