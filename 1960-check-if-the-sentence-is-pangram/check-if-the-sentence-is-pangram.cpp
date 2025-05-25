474800e509be76d5576f5517ac7f2127de7c4dfc
Harshita Roonwal <roonwal721972@gmail.com>
2025-05-25 10:58:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (84.58%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        int arr[26] = {0};  // Track if each letter 'a' to 'z' is seen
+
+        for (auto ch : sentence) {
+            arr[ch - 'a'] = 1;  // Mark the letter as seen
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] == 0) {
+                return false;  // If any letter is missing, it's not a pangram
+            }
+        }
+
+        return true;  // All letters found
+    }
+};
