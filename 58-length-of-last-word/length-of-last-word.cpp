c47a78a74d4a3ba768c2b46b3b020aef9590a922
Harshita Roonwal <roonwal721972@gmail.com>
2025-05-29 12:44:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (66.02%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int length = 0;
-        bool counting = false;
-        
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s[i] != ' ') {
-                counting = true;
-                length++;
-            }
-            else if (counting) {
-                break;
+        int n = s.size();
+        int count = 0;
+        for(int i = n-1; i>=0; i--){
+            if(s[i] == ' ' && count != 0){//if there is a space at the end of the string this will not become true coz count will be 0 
+            break;
+
+            }else if(s[i] != ' '){//if its a char then it needs to be counted 
+            count++;
+
             }
         }
-        
-        return length;
+        return count;
     }
 };
