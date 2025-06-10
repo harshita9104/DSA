09e1e20450f0561608df12f5f0888eed19fa0fb7
Harshita Roonwal <roonwal721972@gmail.com>
2025-06-10 22:53:57 +0530

    Time: 3 ms (32.15%) | Memory: 12.8 MB (27.75%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    void solve(vector<int>& ip, vector<int>& op, vector<vector<int>>& ans){
-        if(ip.size() == 0){
-            ans.push_back(op);
+    // Main function to generate all possible subsets (the power set)
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;     // This will store all the subsets
+        vector<int> curr;            // Temporary vector to build each subset
+        helper(nums, 0, curr, ans);  // Start recursion from index 0
+        return ans;
+    }
+    
+    // Recursive helper function to generate subsets
+    void helper(vector<int>& nums, int idx, vector<int>& curr, vector<vector<int>>& ans) {
+        // Base case: if we've considered all elements
+        if(idx == nums.size()) {
+            ans.push_back(curr); // Add current subset to the answer
             return;
         }
-        vector<int> op1 = op;
-        vector<int> op2 = op;
-        op2.push_back(ip[0]);
-        vector<int> newIp(ip.begin() + 1, ip.end());
-        solve(newIp, op1, ans);
-        solve(newIp, op2, ans);
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> op;
-        solve(nums, op, ans);
-        return ans;
+        // Exclude the current element and move forward
+        helper(nums, idx + 1, curr, ans);
+        
+        // Include the current element and move forward
+        curr.push_back(nums[idx]);
+        helper(nums, idx + 1, curr, ans);
+        
+        // Backtrack: remove the current element before returning
+        curr.pop_back();
     }
 };
