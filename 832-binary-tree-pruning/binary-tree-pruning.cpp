65f5dc5033adcaf0796ada170120f31b0205e794
Harshita Roonwal <roonwal721972@gmail.com>
2025-06-21 00:41:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (76.46%) - LeetSync

--- 832-binary-tree-pruning/binary-tree-pruning.cpp
+++ 832-binary-tree-pruning/binary-tree-pruning.cpp
@@ -12,12 +12,15 @@
 class Solution {
 public:
     TreeNode* pruneTree(TreeNode* root) {
-        if(root!=NULL){
-            root->left = pruneTree(root->left);
-            root->right = pruneTree(root->right);
-            if(!root->left && !root->right && root->val==0)
-                return NULL;
-        }
+        if(root == NULL)return NULL;
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        //ek root ki poori subtree m check krna hai agar ek bhi 1 hai to NULL return nhi kr skte
+        //or agar us root ki poori subtree m sab 0 hai to NULL return kardo 
+        //or ye sab tabhi karna h jab root ki val 0 h
+        //agar root ki val 1 hai to root hi return karna h
+        //agar below cond m se ek bhi false hua yani ek bhi root ki val 1 hai to uska neg 0 hoga or cond false hogi or   root return hoga 
+        if(!root->left && !root->right && root->val ==0)return NULL;
         return root;
     }
 };
