d13359a0287b783d039dc4ad1b7f525a9fece6a9
Harshita Roonwal <roonwal721972@gmail.com>
2025-07-01 11:28:25 +0530

    Time: 3 ms (19.39%) | Memory: 27.3 MB (31.10%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        //arr k har ek index k liye uska right max elem or left max elem nikalna hai usme se jo smaller hua (uski height - bar height )* width is eq to vol 
+        // left or right max find krna hoga har ek index ka
+        // inme se jo min hoga usme se subtract krna hoga current indes ki height
+        // or agar result pos aaya to vol m add krdo width = 1 so simply add
         int n = height.size();
         vector<int> left(n);
-        left[0] = 0;
         vector<int> right(n);
-        right[n-1] = 0;
-        for(int i = 1; i<n ; i++){
-            left[i] = max(left[i-1],height[i-1] );
-        }
-        for(int i = n-2; i>=0 ; i--){
-            right[i] = max(right[i+1],height[i+1] );
-        }
-        int volume = 0;
-    for (int i = 0; i < n; i++) {
-        int trapheight = min(left[i], right[i]);
-        int trapped = trapheight - height[i];
-        if (trapped > 0)
-            volume += trapped;
-    }
+        left[0] = 0;//0th index ka left m koi height nhi to max height is 0
+        right[n-1] =0;//last index k right m koi nhi to right max height  =0
 
-    return volume;    
+        for(int i =1; i<n; i++){
+           //current index par jab hm hai to hmare pass prev index ka leftmax or prev index ki height h , ab curr index ka left max find krne k liye prev index ki height or prev index k leftmax m se jo max hai vo 
+           left[i] = max(left[i-1], height[i-1]);
+
+        }
+        for(int i = n-2; i>=0; i--){
+            right[i] = max(right[i+1], height[i+1]);
+        }
+int volume =0;
+        for(int i =0; i<n; i++){
+            int trappingHeight = min(left[i], right[i]);
+            int trappedHeight = trappingHeight - height[i];
+            if(trappedHeight >0)volume += trappedHeight;
+        }
+        return volume;
     }
 };
