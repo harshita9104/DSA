1664af7c115954851afa4135adb0b58ab979e342
Harshita Roonwal <roonwal721972@gmail.com>
2025-07-05 00:01:30 +0530

    Time: 15 ms (21.78%) | Memory: 102.5 MB (29.57%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,37 +1,39 @@
 class Solution {
-private:
-    vector<vector<int>> grid;
-    vector<vector<bool>> visited;
-    int rows, cols;
-
-    int dfs(int i, int j) {
-        if (i < 0 || j < 0 || i >= rows || 
-            j >= cols || grid[i][j] == 0) {
-            return 1;
-        }
-        if (visited[i][j]) {
-            return 0;
-        }
-
-        visited[i][j] = true;
-        return dfs(i, j + 1) + dfs(i + 1, j) + 
-               dfs(i, j - 1) + dfs(i - 1, j);
-    }
-
 public:
+int m;
+int n;
+int peri ;
+void dfs(vector<vector<int>>& grid, int i, int j){
+    //mujhe perimeter tab inc karna hai jab mai ek island par khadi hu or uske neigh m ya to mujhe water mile ya out of bound ja rha ho , in sari condn m aage nhi ja skte to return kar jaana h taaki dusra side ka neigh island explore kr paye
+    if(i<0 || i>= m || j<0|| j>=n || grid[i][j] == 0){
+        peri++;
+        return;
+    }
+    //agar koi island phle se visit kar lia hai to usse -1 kardenge or uspe aagye to vha se return kr jaenge
+    if(grid[i][j] == -1)return;//visited island h already
+    //or else hm ek new island par khade h to neigh call krne se phle usko visited mark krdo
+    grid[i][j] = -1;
+    //har island k 4 neigh hai to charo par dfs call maro 
+    dfs(grid, i+1,j);
+    dfs(grid, i, j+1);
+    dfs(grid, i-1, j);
+    dfs(grid, i, j-1);
+    
+    
+}
     int islandPerimeter(vector<vector<int>>& grid) {
-        this->grid = grid;
-        rows = grid.size();
-        cols = grid[0].size();
-        visited = vector<vector<bool>>(rows, vector<bool>(cols, false));
-
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                if (grid[i][j] == 1) {
-                    return dfs(i, j);
+     m = grid.size();
+        n = grid[0].size();
+        peri =0;
+        for(int i =0; i< m; i++){
+            for(int j =0; j<n; j++){
+                if(grid[i][j] == 1){
+                    //ek island mila to usko pakad k dfs call krdo poora island apne aap traverse ho jaega
+                    dfs(grid, i, j);
+                    return peri;
                 }
             }
         }
-        return 0;
+        return -1;
     }
 };
