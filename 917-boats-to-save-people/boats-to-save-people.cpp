9b4657df6800d0deb95bc604a6c5baf58baeabd3
Harshita Roonwal <roonwal721972@gmail.com>
2025-07-20 19:21:03 +0530

    Time: 23 ms (20.28%) | Memory: 45.8 MB (70.34%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -2,24 +2,33 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(), people.end());
+        int i =0;
+        int j = people.size()-1;
+        int boats=0;
         int n = people.size();
-        int l =0;
-        int h = n-1;
-        int boats = 0;
-        while(l<=h){
-            if(people[l] + people[h] <= limit){
+        vector<bool> inside(n, false);
+        //jab 1 boat m 2 log daalne h 
+        while(i<j){
+            
+            if(people[i] + people[j] <=limit){
+                boats++;
+                inside[i] = inside[j] = true;
+                i++;
+                j--;
                 
-                l++;
-                h--;
-            }else {
-                // Heaviest person goes alone
-                h--;
+            }else if(people[i] + people[j] >limit){
+                j--;
+            }else{
+                continue;
             }
-            boats++;
-
         }
-        //after this we have obtained the no of boats that will take 2 people now the no. of ppl remaining cant be paired up as they exceed tht limit so boat will take them individually
-        
+        //agar pair bn paate to upar ban gye or un pairs ko inside boat true mark kar diya 
+        //ab jo bach gye uko indi boat m daldo
+        for(int i =0; i<n; i++){
+            if(inside[i] == false && people[i] <= limit){
+                boats++;
+            }
+        }
         return boats;
     }
 };
