647351f3ec13394d47728665fe62de081b5cc91f
Harshita Roonwal <roonwal721972@gmail.com>
2025-07-27 23:05:44 +0530

    Time: 919 ms (72.39%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+#no need of join as one table register is sufficient to find the no of distinct users registered for a contest if we grp by contest_id 
+# there may be some users who have not registered for any contest so, for getting the count of total users to find percentage register table ko to group by kr dia ab to operate separately on users table use subquery 
 SELECT contest_id , round(COUNT(DISTINCT user_id)*100/ (SELECT COUNT(user_id) FROM Users),2) AS percentage
 FROM Register 
 
