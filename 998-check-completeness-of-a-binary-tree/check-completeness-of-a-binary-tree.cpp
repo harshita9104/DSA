f28dbe1c46c4b7b61a3e39ae70cdbd953741fd02
Harshita Roonwal <roonwal721972@gmail.com>
2025-08-07 12:54:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (25.51%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
@@ -11,27 +11,25 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root, unsigned long long index, int &count, unsigned long long &maxIndex) {
-        if(root == NULL) return;
-
-        count++;
-        maxIndex = max(maxIndex, index);
-
-        dfs(root->left, 2 * index, count, maxIndex);
-        dfs(root->right, 2 * index + 1, count, maxIndex);
-    }
-
     bool isCompleteTree(TreeNode* root) {
-        int count = 0;
-        unsigned long long maxIndex = 0;
-
-        dfs(root, 1, count, maxIndex);
-
-        return maxIndex == count;
+        queue<TreeNode*> q;
+         bool flag = false;
+         q.push(root);
+        //agar null mila to isko true kardena 
+        //null milne k baad agar koi not null value milti h level order traversal m iska mtlb binary tree is not complete binary tree
+        while(!q.empty()){
+            TreeNode* curr = q.front();
+            q.pop();
+            if(curr == NULL)flag = true;
+            if(curr != NULL){
+                if(flag == true){
+                    return false;
+                }else{
+                    q.push(curr->left);
+                    q.push(curr->right);
+                }
+            }
+        }
+        return true;
     }
 };
-        //for a ith node i starting from 1 its left child will be at 2*i node and its right child will be at 2*i+1 node in array seq of level order traversal of tree
-        // so first we will count total no of nodes in this tree
-        //and if the node is null at any index then we need to keep that empty
-        //so if the arr.size() > total nodes it means a index is empty in arr and tree is not complete
-        
