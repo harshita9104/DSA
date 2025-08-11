41e44e946412a5ff4847c01dd105e9f6ec183bbd
Harshita Roonwal <roonwal721972@gmail.com>
2025-08-12 05:00:24 +0530

    Time: 8 ms (83.48%) | Memory: 12.5 MB (75.77%) - LeetSync

---
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        //We use dynamic programming to find the length of the largest divisible subset ending at each index. We maintain a dp array where dp[i] represents the length of the largest divisible subset ending at index i.
+        int n=nums.size(), maxi=1, num=-1;
+        vector<int>v;
+        sort(nums.begin(), nums.end());
+        vector<int>dp(n, 1);
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(!(nums[i]%nums[j]) && dp[i]<dp[j]+1){
+                    dp[i]=dp[j]+1;
+                    if(maxi<dp[i]){
+                        maxi=dp[i];
+                    }
+                }
+            }
+        }
+        for(int i=n-1; i>=0; i--){
+            if(maxi==dp[i] && (num==-1 || !(num%nums[i]))){
+                v.push_back(nums[i]);
+                maxi--;
+                num=nums[i];
+            }
+        }
+        return v;
+    }
+};
