387fc1ec1d62626862c2f6cc226c7a9dc626b10f
Harshita Roonwal <roonwal721972@gmail.com>
2025-08-25 00:32:18 +0530

    Time: 8 ms (56.60%) | Memory: 55.8 MB (56.09%) - LeetSync

--- 355-design-twitter/design-twitter.cpp
+++ 355-design-twitter/design-twitter.cpp
@@ -1,50 +1,46 @@
 class Twitter {
-private:    
-   unordered_map<int, set<int>> fo;
-   unordered_map<int, vector<pair<int, int>>> t;
-   long long time; 
-
 public:
-/** Initialize your data structure here. */
-Twitter() {
-    time = 0;
-}
-
-/** Compose a new tweet. */
-void postTweet(int userId, int tweetId) {
-    t[userId].push_back({time++, tweetId});
-}
-
-/** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
-vector<int> getNewsFeed(int userId) {
-    priority_queue<pair<int, int>> maxHeap; 
-    for (auto it=t[userId].begin();it!=t[userId].end();++it)
-        maxHeap.push(*it);
-    for (auto it1=fo[userId].begin();it1!=fo[userId].end();++it1){
-        int usr = *it1; // get target user
-        for (auto it2=t[usr].begin();it2!=t[usr].end();++it2)
-            maxHeap.push(*it2);
-    }   
-    vector<int> res;
-    while(maxHeap.size()>0) {
-        res.push_back(maxHeap.top().second);
-        if (res.size()==10) break;
-        maxHeap.pop();
+long long time;
+unordered_map<int,vector<pair<int, int>>> tweets;//key is userid , value is list of pair of time and tweetid
+unordered_map<int, set<int>> following;//key is user id and value is set of users this user followes
+    Twitter() {
+        time =0;
     }
-    return res;
-}
-
-/** Follower follows a followee. If the operation is invalid, it should be a no-op. */
-void follow(int followerId, int followeeId) {
-    if (followerId != followeeId)
-        fo[followerId].insert(followeeId);
-}
-
-/** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
-void unfollow(int followerId, int followeeId) {
-    fo[followerId].erase(followeeId);
-}
+    
+    void postTweet(int userId, int tweetId) {
+        tweets[userId].push_back({time++,tweetId});
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        vector<int> ans;
+        priority_queue<pair<int, int>> maxheap;
+        for(auto it: tweets[userId]){
+            //we are iterating over the list of tweets the userId has made
+            maxheap.push(it);
+        }
+        //now we need to iterate over the tweets of the users this userId followes
+        for(int follow : following[userId]){
+            for(auto it: tweets[follow]){
+                maxheap.push(it);
+            }
+        }
+        while(!maxheap.empty() && ans.size()< 10){
+            int tweet = maxheap.top().second;
+            ans.push_back(tweet);
+            maxheap.pop();
+        }
+        return ans;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        following[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        following[followerId].erase(followeeId);
+    }
 };
+
 /**
  * Your Twitter object will be instantiated and called as such:
  * Twitter* obj = new Twitter();
