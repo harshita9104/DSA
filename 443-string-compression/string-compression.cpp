53a8420f745846a4ee0bfe5cf95941700ef8da6e
Harshita Roonwal <roonwal721972@gmail.com>
2025-09-12 20:47:46 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (77.52%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int i =0;
-        int ansindex=0;
-        int count =0;
+        int i = 0;
+        //kyuki alg se space nhi leni h hm jo resulting char or count h vo ansIndex p daalte jaenge chars arr m hi
+        int ansindex = 0;
         int n = chars.size();
         while(i<n){
-            int j = i+1;//j will itertae and check the similar char count where i will stand on the first occ of unique char
-            chars[ansindex] = chars[i];
+            
+            //kyuki hme ek char ko uske aage vale index elem se compare karna h agar same hai to j++ kardo or uske aage vale elem ko bhi check karo 
+            int j = i+1;
+            //j, arr k baahar point nhi karna chahiye or j++ tab tak karna h jab tak char[i] == chars[j] hai hm 
             while(j<n && chars[i] == chars[j]){
-                
                 j++;
             }
-            int count = j-i;//this will give the count for each char as j will be on the new unique char and i will be on the 1st char of prev char 
-            ansindex++;
+            //old cchar char arr k ansindex p daalo or ansindex ++ karo
+            chars[ansindex++] = chars[i];
+            //ye loop k bahar tak aayenge jab ya to poora loop traverse ho gya hoga ya phir ek bya elem arr occur hua h j ab us naye elem p hi point kar rha 
+            //ab old char ka count ansindex p daalna h
+            int count = j-i;
+            //ab count ko char arr m tab hi daalna h agar vo >1 hai or count integer agar 2 ya 3 digit ho to phle usko single digit m convert karke phir ek-ek karke char arr m daalna
+             
             if(count>1){
-                string num = to_string(count);//imp if the count is more than 1 char
-                for(char ch : num){
-                    //iterate over this string which has count and enter each digit one by one
-                    chars[ansindex] = ch;
-                    ansindex++;
+                //convert count integer to string
+                string cnt = to_string(count);
+                //now traverse thr each character of this string and put it in char arr one by one
+                for(char ch: cnt){
+                    chars[ansindex++] = ch;
                 }
 
             }
-            //since j would be pointing to new unique char make i also point there
+            // ab new elem jo ki j p point kar rha h i ko vha leke jao taaki new char or uske count ko bhi char arr m daal aao
+
             i = j;
 
+
         }
-        return  ansindex;
+        return ansindex;
+        
+
 
     }
 };
