ca0f8d8a038a27d9cef7156868e6e75760c9d2a7
Harshita Roonwal <roonwal721972@gmail.com>
2025-09-12 20:52:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (84.54%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        // int arr[26] = {0};  // Track if each letter 'a' to 'z' is seen
+        int arr[26] = {0};  // Track if each letter 'a' to 'z' is seen
 
-        // for (auto ch : sentence) {
-        //     arr[ch - 'a'] = 1;  // Mark the letter as seen
-        // }
-
-        // for (int i = 0; i < 26; i++) {
-        //     if (arr[i] == 0) {
-        //         return false;  // If any letter is missing, it's not a pangram
-        //     }
-        // }
-
-        // return true;  // All letters found
-       
-       
-
-
-        //since we just need to check if the sentence contain all 26 char of alphabet or not so we use a set of char and since set stores only unique elem we will insert all elem of sentence in it and check in the end if the size of set is 26 or not
-        set<char> unique;
-        for(auto ch: sentence){
-            unique.insert(ch);
+        for (auto ch : sentence) {
+            arr[ch - 'a'] = 1;  // Mark the letter as seen
         }
-        if(unique.size() == 26){
-            return true;
-        }else{
-            return false;
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] == 0) {
+                return false;  // If any letter is missing, it's not a pangram
+            }
         }
+
+        return true;  // All letters found
+
     }
 };
