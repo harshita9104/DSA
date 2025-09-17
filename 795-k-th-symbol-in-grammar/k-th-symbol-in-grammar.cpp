130c728701eb034d73dba0b4c0adcbd4c30d3f46
Harshita Roonwal <roonwal721972@gmail.com>
2025-09-18 02:04:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (49.77%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -1,38 +1,28 @@
-
-//recursive approach
-    //    int mid = (pow(2,n-1))/2;
-    //    if(n == 1 && k == 1){
-    //     return 0;
-    //    }
-    //    if(k<= mid){
-    //     return kthGrammar(n-1,  k);
-    //    }else 
-    //     return  (!kthGrammar( n -1 , k - mid));
-       
-
-       class Solution {
+class Solution {
 public:
     int kthGrammar(int n, int k) {
-        // Initialize a flag to track if the values of k and first element are the same.
-        bool areValuesSame = true; 
+        //recursion m induction, base case , hypothesis
+        //hypothesis m smaller input k liye recursive call krte hai , 
+        //and we assume that we will get the answer , here the value in table for that input row and col , then we append somthing in that output of smaller input, and then get the output for original input
+        //now issue is how we will do smaller input here
+        //as we can do n-1 , but for k we need to think
+        //in this problem upon observation 
+        //we can see that the no of elem in a row is 2 ki pow n-1
+        //and these elem are just getting double in next row
+        //for nth row and the elem remains same as n-1 row till half
+        //then second half of nth row has the number as the complement of num on 1st half of n-1 row
+        //rec call for (n, k)  can be reduced to rec call for(n-1, k) if k is in 1st half 
+        //and if k is in second half, if k> mid
+        //then solve(n, k) = !solve(n-1,k-mid);
 
-        // Calculate the total number of elements in the nth row, which is 2^(n-1).
-        n = pow(2, n - 1);
-
-        // Continue until we reach the first row.
-        while (n != 1) {
-            // Halve the number of elements in the row.
-            n /= 2;
-
-            // If k is in the second half of the row, adjust k and toggle the flag.
-            if (k > n) {
-                k -= n;
-                areValuesSame = !areValuesSame;
-            }
-        }
-
-        // Return 0 if the flag indicates that the values are the same; otherwise, return 1.
-        return ((areValuesSame) ? 0 : 1) ;
+        //base cond is given in q that for n =1 , k =1 the val is 0
+        if(n == 1 && k == 1)return 0;
+        int mid = pow(2, n-1)/2;
+        if(k<= mid)return kthGrammar(n-1, k);
+        else 
+         return  (!kthGrammar(n-1, k-mid));
+        
+        
 
     }
 };
