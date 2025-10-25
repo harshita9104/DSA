eb8a98b50b7848f8f45214e65e9033badc51a397
Harshita Roonwal <roonwal721972@gmail.com>
2025-10-25 11:55:48 +0530

    Time: 7 ms (44.88%) | Memory: 15.9 MB (5.99%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //in order to do this question in O(1) TC u can use hashmap which stores elem, index in map
-        map<int,int> mp;
-        //standing at one elem we will look for the rem = target- curr elem in map if we are able to find it we will return the index of those elem if not we will insert curr elem and its coresponding index in map and move to the next elem
-        int n = nums.size();
-        for(int i = 0; i<n; i++){
-            int elem = target - nums[i];
-            if(mp.find(elem) != mp.end()){//agar map m elem find krne par map k end m point nhi kr rha h it means vo elem map m exist krta hai 
-            return{i,mp[elem]};
-
+        unordered_map<int, int> mp;//{number, its index}
+        vector<int> ans;
+        for(int i = 0; i< nums.size(); i++){
+            int rem = target - nums[i];
+            if(mp.find(rem) != mp.end()){
+               ans.push_back(mp[rem]);
+               ans.push_back(i);
+            }else{
+                mp[nums[i]] = i;
             }
-            mp[nums[i]] =i;//insert value(index) coresponding to key (elem)
         }
-        return {};
+        return ans;
     }
 };
