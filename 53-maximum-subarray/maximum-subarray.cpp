8729d18d188a6473ca369042237f7e9995c06ab5
Harshita Roonwal <roonwal721972@gmail.com>
2025-11-12 02:08:02 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (80.41%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,13 +1,24 @@
-
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxsum = nums[0];
-        int curr = nums[0];
-        for(int i = 1; i < nums.size(); i++) {
-            curr = max(nums[i], curr + nums[i]);
-            maxsum = max(maxsum, curr);
+        int n = nums.size();
+        int maxprofit =INT_MIN;
+        int currsum =0;
+        // for(int start = 0; start<n; start++){
+        //     int currsum =0;//jaise hi starting point change ho rha currsum =0 krdo
+        //     for(int end = start ; end<n; end++){
+        //        currsum += nums[end];
+        //        maxprofit = max(maxprofit, currsum);
+        //     }
+        // }
+        //ek hi loop chalao jaise hi sum neg ho hm usko aage carry nhi krenge sum =0 kr denge it means we are starting new subarr from next elem 
+        for(int i = 0; i<n; i++){
+            currsum += nums[i];
+             maxprofit = max(maxprofit, currsum);
+             if(currsum<0){
+                currsum =0;
+             }
         }
-        return maxsum;
+        return maxprofit;
     }
 };
