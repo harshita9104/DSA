5a52fed572300adfbe49a31527100c7666f49cbe
Harshita Roonwal <roonwal721972@gmail.com>
2026-06-22 21:41:53 +0530

    Time: 14 ms (74.29%) | Memory: 26.1 MB (48.67%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        //question is how we will figure out that two string are anagram 
-        //if we sort all the strings then all the anagrams will become one string becoz they all consist of same char in same number ,
-        // and then we will store the anagram strings corresponding to their sorted val in map to segregate them from other set of anagrams
-        unordered_map<string,vector<string>> mp;
-
-        for(int i =0; i<strs.size(); i++){
-             string temp = strs[i];//temp ko sort karenge 
+        unordered_map<string, vector<string>> mp;
+        vector<vector<string>> result;
+        for(int i =0; i< strs.size(); i++){
+            string temp = strs[i];
             sort(temp.begin(), temp.end());
             mp[temp].push_back(strs[i]);
         }
-        //ab map m sorted val k correspoding uske anagrams ka vector hai to map m iterate karke ye vector ko result m daaldo
-        vector<vector<string>> result;
-        for(auto  it : mp){
-            result.push_back(it.second);//iterator se map ki value ko access kro 
+
+        for(auto it : mp){
+            result.push_back(it.second);
         }
         return result;
     }
